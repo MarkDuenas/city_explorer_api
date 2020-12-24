e646bd8c4ddceb9a8e7c68b26050743ec50d5745
Mark Duenas <markgdlduenas@gmail.com>
2020-12-23 19:54:15 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
+
+
+app.listen(PORT, () => console.log(`Now rocking on port ${PORT}`));
