af132978a788ca1e2d882029f890845ff676ba0e
Mark Duenas <markgdlduenas@gmail.com>
2021-01-02 13:03:33 -0800

    added weather route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 
 const PORT = process.env.PORT;
 const app = express();
@@ -11,7 +12,7 @@ app.use(cors());
 
 // ROUTES
 app.get('/location', handleLocation);
-
+app.get('/weather', handleWeather);
 
 
 // HELPER FUNCTIONS
@@ -22,12 +23,26 @@ function handleLocation(req, res) {
     res.send(locationData);
 }
 
+function handleWeather(req, res) {
+    const wData = require('./data/weather.json');
+    const weatherData = [];
+    wData.data.forEach( entry => {
+        weatherData.push(new Weather(entry));
+    });
+    res.send(weatherData);
+}
+
 // CONSTRUCTORS
 function Location(city, geoData) {
     this.search_query = city;
     this.formatted_query = geoData[0].display_name;
     this.latitude = geoData[0].lat;
     this.longitude = geoData[0].lon;
+}
+
+function Weather(wData) {
+    this.forecast = wData.weather.description;
+    this.time = wData.valid_date;
 }
 
 app.listen(PORT, () => console.log(`Now rocking on port ${PORT}`));
